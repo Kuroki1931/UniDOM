4e3dacbd6d2b10d1cb08ed82ab3030f9b2244556
Kuroki So <so.kuroki1931@gmail.com>
2023-04-09 12:49:42 +0900

    success connect plasitince and gazebo

---
+++ real2sim/real2sim/util/lang_goal.py
@@ -0,0 +1,381 @@
+LANG_GOAL = {
+    "Rollingpin-v1": {
+        'num_steps': 80,
+        0: {
+            "command": "Crush the whole surface of the box",
+            "object": "box",
+            "manipulation": "crush",
+            "location": "whole surface"
+        },
+        1: {
+            "command": "Push the entire section of the object",
+            "object": "object",
+            "manipulation": "push",
+            "location": "entire section"
+        },
+        2: {
+            "command": "Flatten whole section of the plasticine",
+            "object": "plasticine",
+            "manipulation": "flatten",
+            "location": "whole section",
+        },
+        3: {
+            "command": "Apply force to whole surface of the soft body",
+            "object": "soft body",
+            "manipulation": "apply force",
+            "location": "whole surface"
+        },
+        4: {
+            "command": "Flatten entire section of the object",
+            "object": "object",
+            "manipulation": "flatten",
+            "location": "entire section"
+        },
+        5: {
+            "command": "Apply force to whole surface of the box",
+            "object": "box",
+            "manipulation": "apply force",
+            "location": "whole surface"
+        },
+        6: {
+            "command": "Push whole surface of the soft body",
+            "object": "soft body",
+            "manipulation": "push",
+            "location": "whole surface",
+        },
+        7: {
+            "command": "Crush whole surface of the soft body",
+            "object": "soft body",
+            "manipulation": "crush",
+            "location": "whole surface"
+        },
+    },
+    "Rollingpin-v2": {
+        'num_steps': 80,
+        0: {
+            "command": "crush left side of the box",
+            "object": "box",
+            "manipulation": "crush",
+            "location": "left side",
+        },
+        1: {
+            "command": "Push the left side of the object",
+            "object": "object",
+            "manipulation": "push",
+            "location": "left side"
+        },
+        2: {
+            "command": "Compress left flank of the soft bod",
+            "object": "soft body",
+            "manipulation": "compress",
+            "location": "left flank",
+
+        },
+        3: {
+            "command": "flatten left-hand side of the plasticine",
+            "object": "plasticine",
+            "manipulation": "flatten",
+            "location": "left-hand side",
+        },
+        4: {
+            "command": "Apply pressure to left flank of the box",
+            "object": "box",
+            "manipulation": "apply pressure",
+            "location": "left flank"
+        },
+        5: {
+            "command": "Flatten left portion of the box",
+            "object": "box",
+            "manipulation": "flatten",
+            "location": "left portion"
+        },
+        6: {
+            "command": "Crush left side of the object",
+            "object": "object",
+            "manipulation": "crush",
+            "location": "left side",
+        },
+        7: {
+            "command": "Compress left-hand side of the plasticine",
+            "object": "plasticine",
+            "manipulation": "compress",
+            "location": "left-hand side"
+        },
+    },
+    "Rollingpin-v3": {
+        'num_steps': 80,
+        0: {
+            "command": "Crush the right side of the soft object",
+            "object": "soft object",
+            "manipulation": "crush",
+            "location": "right side"
+        },
+        1: {
+            "command": "Apply force to right surface of the box",
+            "object": "box",
+            "manipulation": "apply force",
+            "location": "right surface",
+        },
+        2: {
+            "command": "Flatten right portion of the box",
+            "object": "box",
+            "manipulation": "flatten",
+            "location": "right portion"
+        },
+        3: {
+            "command": "Push right flank of the plasticine",
+            "object": "plasticine",
+            "manipulation": "push",
+            "location": "right flank"
+        },
+        4: {
+            "command": "Flatten right flank of the plasticine",
+            "object": "plasticine",
+            "manipulation": "flatten",
+            "location": "right flank"
+        },
+        5: {
+            "command": "Apply pressure to right surface of the box",
+            "object": "box",
+            "manipulation": "apply pressure",
+            "location": "right surface"
+        },
+        6: {
+            "command": "Push right flank of the soft body",
+            "object": "soft body",
+            "manipulation": "push",
+            "location": "right flank",
+        },
+        7: {
+            "command": "Compress right portion of the box",
+            "object": "box",
+            "manipulation": "compress",
+            "location": "right portion"
+        },
+    },
+    "Rollingpin-v4": {
+        'num_steps': 70,
+        0: {
+            "command": "Crush the center of the object",
+            "object": "object",
+            "manipulation": "crush",
+            "location": "center"
+        },
+        1: {
+            "command": "Apply pressure to the center of the plasticine",
+            "object": "plasticine",
+            "manipulation": "apply pressure",
+            "location": "center"
+        },
+        2: {
+            "command": "push center surface of the box",
+            "object": "box",
+            "manipulation": "push",
+            "location": "center surface",
+        },
+        3: {
+            "command": "Compress center area of the soft object",
+            "object": "soft object",
+            "manipulation": "compress",
+            "location": "center area"
+        },
+        4: {
+            "command": "Push center area of the soft object",
+            "object": "soft object",
+            "manipulation": "push",
+            "location": "center area"
+        },
+        5: {
+            "command": "Crush center surface of the plasticine",
+            "object": "plasticine",
+            "manipulation": "crush",
+            "location": "center surface"
+        },
+        6: {
+            "command": "Apply pressure to the center of the box",
+            "object": "box",
+            "manipulation": "apply pressure",
+            "location": "center",
+        },
+        7: {
+            "command": "Crush center surface of the object",
+            "object": "object",
+            "manipulation": "crush",
+            "location": "center surface"
+        },
+    },
+    "Rope-v1": {
+        'num_steps': 40,
+        0: {
+            "command": "cut the center of the object",
+            "object": "",
+            "manipulation": "",
+            "location": ""
+        },
+    },
+    "Rope-v2": {
+        'num_steps': 35,
+        0: {
+            "command": "cut the left side of the object",
+            "object": "",
+            "manipulation": "",
+            "location": ""
+        },
+    },
+    "Rope-v3": {
+        'num_steps': 50,
+        0: {
+            "command": "cut the right side of the object",
+            "object": "",
+            "manipulation": "",
+            "location": ""
+        },
+    },
+    "Torus-v1": {
+        'num_steps': 30,
+        0: {
+            "command": "torus the center of the object",
+            "object": "",
+            "manipulation": "",
+            "location": ""
+        },
+    },
+    "Torus-v2": {
+        'num_steps': 35,
+        0: {
+            "command": "torus the left side of the object",
+            "object": "",
+            "manipulation": "",
+            "location": ""
+        },
+    },
+    "Torus-v3": {
+        'num_steps': 35,
+        0: {
+            "command": "torus the right side of the object",
+            "object": "",
+            "manipulation": "",
+            "location": ""
+        },
+    },
+    "Table-v1": {
+        'num_steps': 40,
+        0: {
+            "command": "slice the left side of the object",
+            "object": "",
+            "manipulation": "",
+            "location": ""
+        },
+    },
+    "Table-v2": {
+        'num_steps': 40,
+        0: {
+            "command": "slice the right side of the object",
+            "object": "",
+            "manipulation": "",
+            "location": ""
+        },
+    },
+}
+
+
+
+ACTION = {
+    'Rope': {
+        'order': [0, 2, 1],
+        'direction': [-1, 1, -1],
+    },
+    'Torus': {
+        'order': [0, 2, 1],
+        'direction': [-1, 1, -1],
+    },
+    'Table': {
+        'order': [0, 2, 1],
+        'direction': [-1, 1, -1],
+    },
+    'Chopsticks': {
+        'order': [0, 2, 1],
+        'direction': [-1, 1, -1],
+    },
+    'Move': {
+        'order': [0, 2, 1],
+        'direction': [-1, 1, -1],
+    },
+}
+
+'''
+Rolling x, rotate, z
+Torus, Rope x, z, y
+'''
+
+
+# OLD_LANG_GOAL = {
+#     'Rollingpin-v1': {
+#         'lang': [
+#             'crush the object',
+#         ],
+#         'num_steps': 80,
+#     },
+#     'Rollingpin-v2': {
+#         'lang': [
+#             'crush the left side of the object',
+#         ],
+#         'num_steps': 80,
+#     },
+#     'Rollingpin-v3': {
+#         'lang': [
+#             'crush the right side of the object',
+#         ],
+#         'num_steps': 80,
+#     },
+#     'Rollingpin-v4': {
+#         'lang': [
+#             'crush the center of the object',
+#         ],
+#         'num_steps': 70,
+#     },
+#     'Rollingpin-v5': {
+#         'lang': [
+#             'crush the object sparingly',
+#         ],
+#         'num_steps': 80,
+#     },
+#     'Rope-v1': {
+#         'lang': [
+#             'cut the center of the object',
+#         ],
+#         'num_steps': 40,
+#     },
+#     'Rope-v2': {
+#         'lang': [
+#             'cut the left side of the object',
+#         ],
+#         'num_steps': 35,
+#     },
+#     'Rope-v3': {
+#         'lang': [
+#             'cut the right side of the object',
+#         ],
+#         'num_steps': 50,
+#     },
+#     'Torus-v1': {
+#         'lang': [
+#             'torus the center of the object',
+#         ],
+#         'num_steps': 30,
+#     },
+#     'Torus-v2': {
+#         'lang': [
+#             'torus the left side of the object',
+#         ],
+#         'num_steps': 35,
+#     },
+#     'Torus-v3': {
+#         'lang': [
+#             'torus the right side of the object',
+#         ],
+#         'num_steps': 35,
+#     },
+# }
+
+
