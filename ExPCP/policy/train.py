afb899e5dd91783392ab6db5113d087aad770bf1
Kuroki So <so.kuroki1931@gmail.com>
2023-05-01 12:44:31 +0900

    data create pipeline

--- ExPCP/policy/train.py
+++ ExPCP/policy/train.py
@@ -18,10 +18,8 @@ from pathlib import Path
 from clip import tokenize
 
 from tqdm import tqdm
-from util.clip import build_model, load_clip
 from models.cls_ssg_model import CLS_SSG_Model
 from PIL import Image
-from util.lang_goal import LANG_GOAL
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = BASE_DIR
@@ -53,10 +51,10 @@ def parse_args():
     parser.add_argument('--epoch', default=10000, type=int, help='number of epoch in training')
     parser.add_argument('--save_epoch', default=200, type=int, help='save epoch')
     parser.add_argument('--learning_rate', default=0.001, type=float, help='learning rate in training')
-    parser.add_argument('--num_plasticine_point', type=int, default=3072, help='Point Number of Plasticine')
-    parser.add_argument('--num_primitive_point', type=int, default=1024, help='Point Number of Primitive')
+    parser.add_argument('--num_plasticine_point', type=int, default=3000, help='Point Number of Plasticine')
+    parser.add_argument('--num_goal_point', type=int, default=2600, help='Point Number of Primitive')
     parser.add_argument('--optimizer', type=str, default='Adam', help='optimizer for training')
-    parser.add_argument('--experts_dir', type=str, default='2023-03-20_14-57', help='experiment root')
+    parser.add_argument('--experts_dir', type=str, default='2023-05-01_03-25', help='experiment root')
     parser.add_argument('--decay_rate', type=float, default=1e-4, help='decay rate')
     parser.add_argument('--use_normals', action='store_true', default=False, help='use normals')
     parser.add_argument('--process_data', action='store_true', default=False, help='save data offline')
@@ -66,7 +64,7 @@ def parse_args():
     parser.add_argument('--command_num', type=int, default=4, help='command num')
     
     parser.add_argument("--algo", type=str, default='action')
-    parser.add_argument("--env_name", type=str, default="Rollingpin-v1")
+    parser.add_argument("--env_name", type=str, default="Move-v1")
     parser.add_argument("--seed", type=int, default=0)
     parser.add_argument("--sdf_loss", type=float, default=500)
     parser.add_argument("--density_loss", type=float, default=500)
@@ -94,7 +92,7 @@ def load_dataset(in_file, batch_size, num_point):
 		in_features = {
 			'points': tf.io.FixedLenFeature([num_point * 3], tf.float32),
 			'vector_encode': tf.io.FixedLenFeature([num_point * 5], tf.float32),
-			'lang': tf.io.FixedLenFeature([LANG_EMBEDDING_SIZE], tf.float32),
+			'parameters': tf.io.FixedLenFeature([3], tf.float32),
 			'action': tf.io.FixedLenFeature([3], tf.float32)
 		}
 
@@ -104,13 +102,13 @@ def load_dataset(in_file, batch_size, num_point):
 
 		points = sample['points']
 		vector_encode = sample['vector_encode']
-		lang = sample['lang']
+		parameters = sample['parameters']
 		action = sample['action']
 
 		points = tf.reshape(points, (num_point, 3))
 		vector_encode = tf.reshape(vector_encode, (num_point, 5))
 
-		return points, vector_encode, lang, action
+		return points, vector_encode, parameters, action
 
 	dataset = tf.data.TFRecordDataset(in_file)
 	dataset = dataset.shuffle(shuffle_buffer)
@@ -145,40 +143,16 @@ def train(args):
         print(str)
     log_string('PARAMETER ...')
     log_string(args)
-    
-    '''CLIP'''
-    clip_model, _ = load_clip("RN50", jit=False)
-    clip_rn50 = build_model(clip_model.state_dict())
-    clip_rn50 = clip_rn50.float()
-    clip_rn50.eval()
-    def clip_emb(lang):
-        tokens = tokenize([lang]).numpy()
-        token_tensor = torch.from_numpy(tokens)
-        lang_goal_emb, lang_token_embs = clip_rn50.encode_text_with_embeddings(token_tensor)
-        return lang_goal_emb[0].to('cpu').detach().numpy().copy()
-    
+
     '''env'''
     set_random_seed(args.seed)
-    env1 = make('Rollingpin-v1', nn=(args.algo=='nn'), sdf_loss=args.sdf_loss,
+    env = make(args.env_name, nn=(args.algo=='nn'), sdf_loss=args.sdf_loss,
 								density_loss=args.density_loss, contact_loss=args.contact_loss,
 								soft_contact_loss=args.soft_contact_loss)
-    env1.seed(args.seed)
-    env2 = make('Rope-v1', nn=(args.algo=='nn'), sdf_loss=args.sdf_loss,
-								density_loss=args.density_loss, contact_loss=args.contact_loss,
-								soft_contact_loss=args.soft_contact_loss)
-    env2.seed(args.seed)
-    env3 = make('Torus-v1', nn=(args.algo=='nn'), sdf_loss=args.sdf_loss,
-								density_loss=args.density_loss, contact_loss=args.contact_loss,
-								soft_contact_loss=args.soft_contact_loss)
-    env3.seed(args.seed)
-    env4 = make('Table-v1', nn=(args.algo=='nn'), sdf_loss=args.sdf_loss,
-								density_loss=args.density_loss, contact_loss=args.contact_loss,
-								soft_contact_loss=args.soft_contact_loss)
-    env4.seed(args.seed)
-    env_dict = {'Rollingpin': env1, 'Rope': env2, 'Torus': env3, 'Table': env4}
-
+    env.seed(args.seed)
+   
     action_size = 3
-    num_point = args.num_primitive_point + args.num_plasticine_point
+    num_point = args.num_plasticine_point + args.num_goal_point
 
     model = CLS_SSG_Model(args.batch_size, action_size)
     train_ds = load_dataset(f'data/{args.experts_dir}/train_experts.tfrecord', args.batch_size, num_point)
@@ -193,7 +167,7 @@ def train(args):
 			f'{exp_dir}/model/weights.ckpt', 'mean_squared_error', save_weights_only=True, save_best_only=True)
 	]
 
-    model.build([(args.batch_size, num_point, 3), (args.batch_size, num_point, 5), (args.batch_size, LANG_EMBEDDING_SIZE)])
+    model.build([(args.batch_size, num_point, 3), (args.batch_size, num_point, 5), (args.batch_size, 3)])
     print(model.summary())
 
     model.compile(
@@ -214,36 +188,18 @@ def train(args):
 			verbose = 1
 		)
         log_string('mean_squared_error: %4f' % history.history['loss'][0])
-
-        with open(f'data/{args.experts_dir}/unique_env_lang.pickle', mode="rb") as f:
-            env_lang_list = pickle.load(f)
         
         if (epoch+1) % args.save_epoch == 0 or epoch == 0:
-            for env_lang in tqdm(list(env_lang_list)):
-                log_string(f'Test: {env_lang}')
-                test_env_name, index, test_lang, test_object, test_manipulation, test_location = env_lang.split('&&')
-                test_env = test_env_name.split('-')[0]
-                env = env_dict[test_env]
+            for i in tqdm(range(5)):
+                version = 500 + 1
+                log_string(f'Test version: {version}')
                 env.reset()
-                test_lang_ = '_'.join(test_lang.split(' '))
-                num_steps = LANG_GOAL[test_env_name]['num_steps']
                 output_dir = exp_dir.joinpath(f'{test_env_name}/')
                 output_dir.mkdir(exist_ok=True)
                 output_dir = output_dir.joinpath(f'{test_lang_}/')
                 output_dir.mkdir(exist_ok=True)
-                
-                if args.clip_type == 0:
-                    test_lang_goal_emb = pooled_text(test_lang)
-                else:
-                    if args.command_type:
-                        object_lang_goal_emb = clip_emb(test_object)
-                        manipulation_lang_goal_emb = clip_emb(test_manipulation)
-                        location_lang_goal_emb = clip_emb(test_location)
-                        test_lang_goal_emb = np.concatenate([object_lang_goal_emb, manipulation_lang_goal_emb, location_lang_goal_emb])
-                    else:
-                        test_lang_goal_emb = clip_emb(test_lang)
 
-                pc_encode = np.zeros((args.num_plasticine_point + args.num_primitive_point, 2))
+                pc_encode = np.zeros((args.num_plasticine_point + args.num_goal_point, 2))
                 pc_encode[:args.num_plasticine_point, 0] = 1
                 pc_encode[args.num_plasticine_point:, 1] = 1
 
@@ -255,7 +211,7 @@ def train(args):
                         env.step(np.array([0, 0, 0])) # plasticinelab bug?
                         continue
 
-                    test_points = sample_pc(test_plasticine_pc, test_primtiive_pc, args.num_plasticine_point, args.num_primitive_point)
+                    test_points = sample_pc(test_plasticine_pc, test_primtiive_pc, args.num_plasticine_point, args.num_goal_point)
                     vector = test_points - np.mean(test_primtiive_pc, axis=0)
                     vector_encode = np.hstack([vector, pc_encode])
                     act = model.forward_pass([
@@ -265,11 +221,6 @@ def train(args):
 			        ], False, 1)
                     act = act.numpy()[0]
                     print(act)
-
-                    if 'Rollingpin' in test_env_name:
-                        act[0] *= -1
-                        act = act[[0, 2, 1]]
-                    _, _, _, loss_info = env.step(act)
 
                     last_iou = loss_info['incremental_iou']
                     
