f740e5ce792125db7be74502aee523fea127d86b
Kuroki So <so.kuroki1931@gmail.com>
2023-05-02 18:46:54 +0900

    merge

--- ExPCP/policy/pbm/each_make_bc_datasets.sh
+++ ExPCP/policy/pbm/each_make_bc_datasets.sh
@@ -8,15 +8,7 @@ elif [ $(( ${1} % 3)) -eq 2 ]; then
     export CUDA_VISIBLE_DEVICES=2
 fi
 
-<<<<<<< HEAD
 VERSION=$(( ${1}+${4}+269 ))
-=======
-<<<<<<< HEAD
-VERSION=$(( ${1}+${4}+269 ))
-=======
-VERSION=$(( ${1}+${4}+23 ))
->>>>>>> f4e96ed12613cad00e894537c4d2a9bf64d7f9ae
->>>>>>> 204ce3c11db89cf2d9cc059649b606e56f87e818
 NAME="${3}${VERSION}"
 echo $NAME
 
