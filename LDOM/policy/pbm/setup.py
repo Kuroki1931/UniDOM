c41055f14ce3aa9208327671035e937dd070097c
Kuroki So <so.kuroki1931@gmail.com>
2023-06-23 14:58:59 +0900

    lodm

--- LDOM/policy/pbm/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-install_requires = ['scipy', 'numpy', 'torch', 'opencv-python', 'tqdm', 'taichi', 'gym', 'tensorboard', 'yacs', 'baselines']
-
-setup(name='plb',
-      version='0.0.1',
-      install_requires=install_requires,
-      )
